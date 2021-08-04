978aed6d4fb04c4e0686bff9e906d6fda8fd0b9c
whysnakes <88395238+whysnakes@users.noreply.github.com>
2021-08-04 19:19:41 +0800

    Add main randomizer function
    Add randomizer function for button click

--- js/btd6randomizer.js
+++ js/btd6randomizer.js
@@ -50,3 +50,83 @@ function getRandomTowers(noOfTowers, towerType) {
     }
     return randomTowers;
 }
+
+function generateRandomOptions() {
+    let playerCount = parseInt($("#playercount").val());
+    let useRandomMap = $("#randomize_map").is(":checked");
+    let useRandomHero = $("#randomize_heroes").is(":checked");
+    let useRandomMode = $("#randomize_mode").is(":checked");
+    let useRandomTowers= $("#randomize_towers").is(":checked");
+    let restrictTowerType = $("#restrict_tower_type").is(":checked");
+    let maxTowers = $("#max_towers").val();
+
+    if(!useRandomMap && !useRandomHero && !useRandomMode && !useRandomTowers){
+        alert("Please pick a random option");
+    }
+
+    //Clear previous results first
+    $("#random_map_result").addClass("d-none");
+    $("#random_map_result").empty();
+    $("#random_mode_result").addClass("d-none");
+    $("#random_mode_result").empty();
+    
+    
+    for(i = 0; i < 4; i++) {
+        $("#player_" + (i + 1)).addClass("d-none");
+        $("#player_" + (i + 1) + "_hero").addClass("d-none");
+        $("#player_" + (i + 1) + "_hero").empty();
+        for(j = 0; j < 4; j++) {
+            $("#player_" + (i + 1) + "_tower_" + (j + 1)).addClass("d-none");
+            $("#player_" + (i + 1) + "_tower_" + (j + 1)).empty();
+        }
+    }
+    //Output map first
+    if(useRandomMap) {
+        $("#random_map_result").removeClass("d-none");
+        $("#random_map_result").append("Map: " + getRandomMap());
+    }
+    //Then the mode
+    if(useRandomMode) {
+        $("#random_mode_result").removeClass("d-none");
+        $("#random_mode_result").append("Mode: " + getRandomMode());
+    }
+    //Finally each player's options (hero and towers)
+    if(useRandomHero || useRandomTowers)
+    {
+        if(useRandomTowers) {
+            let playerTowers = [];
+            if(restrictTowerType) {
+                let modes = ["primary","military","magic","support"];
+                for(var i = 0; i < playerCount; i++) {
+                    let modeIndex = getRandomInt(0,3);
+                    playerTowerType = modes[modeIndex];
+                    let randomTowersByType = getRandomTowers(maxTowers,playerTowerType);
+                    for(var j = 0; j < randomTowersByType.length; j++) {
+                        playerTowers.push(randomTowersByType[j]);
+                    }
+                }
+            }
+            else {
+                playerTowers = getRandomTowers(maxTowers * playerCount);
+            }
+            
+            for(var i = 0; i < playerCount; i++)
+            {
+                $("#player_" + (i + 1)).removeClass("d-none");
+                if(useRandomHero)
+                {
+                    $("#player_" + (i + 1) + "_hero").removeClass("d-none");
+                    $("#player_" + (i + 1) + "_hero").append("Hero: " + getRandomHero());
+                }
+                if(useRandomTowers)
+                {
+                    for(var j = 0; j < maxTowers; j++) {
+                        $("#player_" + (i + 1) + "_tower_" + (j + 1)).removeClass("d-none");
+                        $("#player_" + (i + 1) + "_tower_" + (j + 1)).append("Tower " + (j + 1) + ": " + playerTowers[i + j]);
+                    }
+                }
+            }
+        }
+    }
+
+};
